2af751a54fd089f307b56ba2249b9e8607a6cf59
Unkn0wncod3 <alexander.geier04@web.de>
2023-02-02 13:10:44 +0100

    Shopping Cart Items

--- Web Engineering 1/java.js
+++ Web Engineering 1/java.js
@@ -6,22 +6,6 @@ function showMessage() {
     }, 2000); // Die Box wird nach 2000 Millisekunden ausgeblendet.
   }
 
-function addToCart(item) {
-  // Check if the local storage is supported by the browser
-  if (typeof(Storage) !== "undefined") {
-    // Get the current cart items from the local storage
-    var currentCart = JSON.parse(localStorage.getItem("cart")) || [];
-    
-    // Add the new item to the cart
-    currentCart.push(item);
-    
-    // Save the updated cart back to the local storage
-    localStorage.setItem("cart", JSON.stringify(currentCart));
-  } else {
-    alert("Local storage is not supported by your browser.");
-  }
-}
-
 function removeFromCart(item) {
     if (typeof(Storage) !== "undefined") {
       var cart = JSON.parse(localStorage.getItem("cart")) || [];
@@ -52,7 +36,7 @@ function showCart() {
     for (var item in cart) {
       var tr = document.createElement("tr");
       var td1 = document.createElement("td");
-      td1.innerHTML = item;
+      td1.innerHTML = cart[item].id;
       tr.appendChild(td1);
       var td2 = document.createElement("td");
       td2.innerHTML = cart[item].size;
@@ -63,9 +47,29 @@ function showCart() {
       tr.appendChild(td3);
 
       var td4 = document.createElement("td");
-      td3.innerHTML = cart[item].price;
+      td4.innerHTML = cart[item].price;
       tr.appendChild(td4);
 
       cartTable.appendChild(tr);
     }
   }
+
+function addToCart(id, size, qty, price) {
+    if (typeof(Storage) !== "undefined") {
+        var cart = JSON.parse(localStorage.getItem("cart")) || {};
+        if (!cart[id]) {
+            cart[id] = {};
+        }
+        cart[id].id = id;
+        cart[id].size = size;
+        cart[id].qty = qty;
+        cart[id].price = price;
+        localStorage.setItem("cart", JSON.stringify(cart));
+    } else {
+        alert("Local storage is not supported by your browser.");
+      }
+}
+
+function clearCart() {
+    localStorage.removeItem("cart");
+}
