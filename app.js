ae4249dd48768695040ae6bd090e51e675ff7ddd
Cristiano <122599966+cristiano605@users.noreply.github.com>
2023-02-16 14:23:57 +0100

    use nodejs and express

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path');
+
+const app = express()
+
+const port = 8000;
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/Projektarbeit.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
